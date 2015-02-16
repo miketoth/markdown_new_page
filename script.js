0de5a26fb17d2c091503b940cb34e0db5b298fa5
Michael Toth <tofudrummer@gmail.com>
2015-02-15 18:36:18 -0800

    Added save and load chrome storage stuff

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function saveFile() {
+    var file = document.getElementById("text-input").value;
+    console.log(file);
+    chrome.storage.sync.set({'md_file': file}, function(){
+        console.log("FILE SAVED!");
+    });
+}
+
+function loadFile() {
+    chrome.storage.sync.get('md_file', function(items) {
+        console.log(items);
+        document.getElementById("text-input").value = items.md_file;
+    });
+}
+
 function Editor(input, preview) {
     this.update = function () {
         preview.innerHTML = markdown.toHTML(input.value);
@@ -9,6 +24,14 @@ function Editor(input, preview) {
     this.update();
 }
 var $ = function (id) { return document.getElementById(id); };
+
+// Get around CSP by selecting element by ID
 var div = document.getElementById("text-div");
-div.innerHTML = '<textarea id="text-input" rows="100" cols="82">Type **Markdown** here.</textarea>'
+div.innerHTML = '<textarea id="text-input" rows="100" cols="75">Type **Markdown** here.</textarea>'
 new Editor($("text-input"), $("preview"));
+
+// Same deal as above but with the buttons
+var saveButton = document.getElementById("save-button");
+var loadButton = document.getElementById("load-button");
+saveButton.addEventListener("click", saveFile, false);
+loadButton.addEventListener("click", loadFile, false);
